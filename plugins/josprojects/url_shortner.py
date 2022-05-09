b275d151f0370ded8a3e268bc3f7aa2c2766e97c
TeamQuto <103844261+TeamQuto@users.noreply.github.com>
2022-05-09 12:29:23 +0530

    Update url_shortner.py

--- plugins/josprojects/url_shortner.py
+++ plugins/josprojects/url_shortner.py
@@ -27,7 +27,7 @@ GPLINKS_API = os.environ.get("GPLINKS_API", "008ccaedd6061ad1948838f410947603de9
 
 reply_markup = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton(text='join projects channel', url='https://telegram.me/josprojects')
+        InlineKeyboardButton(text='Team', url='https://telegram.me/TeamYourX')
         ]]
     )
 
@@ -190,7 +190,7 @@ async def short(link):
     
     # Send the text
     try:
-        shorten_urls += "\n\nmade by @josprojects | @jospsupport 🔥"
+        shorten_urls += "\n\Powered by @YourX🕵️"
         return shorten_urls
     except Exception as error:
         return error
