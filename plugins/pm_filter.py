fcfe12939ceeaebe4b98865d42178872f2238669
TeamQuto <103844261+TeamQuto@users.noreply.github.com>
2022-05-09 11:01:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -474,7 +474,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ],[
             InlineKeyboardButton('Zombies', callback_data='zombies'),
             InlineKeyboardButton('« Back', callback_data='start'),
-            InlineKeyboardButton('Adult Bot 🔞', url='https://t.me/AdultSearchXBot')
+            InlineKeyboardButton('Developer', url='https://t.me/YourX')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
